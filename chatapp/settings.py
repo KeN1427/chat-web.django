af20421437b6e648573c373e4bee8b9a4b257c0d
KeN <ken.t.csai@gmail.com>
2023-02-02 23:05:45 +0900

    remove secret key.

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*@km*z0_ma3ih7_ul3s2hd0pxv37lb1#3&ailm4l)er@2g2n*$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
